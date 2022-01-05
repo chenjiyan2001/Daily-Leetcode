384c5097bc2d59db6ff0f8a4d63bb4b8049902df
cHEn <76734087+chenjiyan2001@users.noreply.github.com>
2022-01-06 01:09:02 +0800

    Update 0001.两数之和.java

--- java/0001.两数之和.java
+++ java/0001.两数之和.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    // 1. O(n) t:1ms(100%) m:39.8M(19%) Hash
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            int x = nums[i];
+            if (map.containsKey(target - x)){
+                return new int[]{map.get(target - x), i};
+            }
+            map.put(x, i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
