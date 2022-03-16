cea506a6383a108a4df27e8a2f52e57b939f5968
chenjiyan2001 <1445366404@qq.com>
2022-03-16 21:21:08 +0800

    22/03/16

---
+++ python/0432.全O(1)的数据结构.py
@@ -0,0 +1,67 @@
+# [题解]1. O(1) t:144ms(90%) O(1) m:30.8M(39%) 双链表+哈希
+class Node:
+    def __init__(self, key="", count=0):
+        self.prev = None
+        self.next = None
+        self.keys = {key}
+        self.count = count
+    
+    def insert(self, node: 'Node') -> 'Node':
+        node.prev = self
+        node.next = self.next
+        node.prev.next = node
+        node.next.prev = node
+        return node
+
+    def remove(self):
+        self.prev.next = self.next
+        self.next.prev = self.prev
+
+class AllOne:
+
+    def __init__(self):
+        self.root = Node()
+        self.root.next = self.root
+        self.root.prev = self.root
+        self.nodes = {}
+
+
+    def inc(self, key: str) -> None:
+        if key not in self.nodes:
+            if self.root.next is self.root or self.root.next.count > 1:
+                self.nodes[key] = self.root.insert(Node(key, 1))
+            else:
+                self.root.next.keys.add(key)
+                self.nodes[key] = self.root.next
+        else:
+            cur = self.nodes[key]
+            nxt = cur.next
+            if nxt is self.root or nxt.count > cur.count + 1:
+                self.nodes[key] = cur.insert(Node(key, cur.count + 1))
+            else:
+                nxt.keys.add(key)
+                self.nodes[key] = nxt
+            cur.keys.remove(key)
+            if len(cur.keys) == 0: 
+                cur.remove()
+
+    def dec(self, key: str) -> None:
+        cur = self.nodes[key]
+        if cur.count == 1: 
+            del self.nodes[key]
+        else:
+            pre = cur.prev
+            if pre is self.root or pre.count < cur.count - 1:
+                self.nodes[key] = cur.prev.insert(Node(key, cur.count - 1))
+            else:
+                pre.keys.add(key)
+                self.nodes[key] = pre
+        cur.keys.remove(key)
+        if len(cur.keys) == 0: 
+            cur.remove()
+
+    def getMaxKey(self) -> str:
+        return next(iter(self.root.prev.keys)) if self.root.prev is not self.root else ""
+
+    def getMinKey(self) -> str:
+        return next(iter(self.root.next.keys)) if self.root.next is not self.root else ""
